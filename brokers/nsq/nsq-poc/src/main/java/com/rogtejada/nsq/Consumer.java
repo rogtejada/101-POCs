565507452d3fbfc561570f4ec6d2e9f4b1821ef5
rogtejada <rogtejada@hotmail.com>
2019-09-09 08:53:30 -0300

    removing unused code

--- brokers/nsq/nsq-poc/src/main/java/com/rogtejada/nsq/Consumer.java
+++ brokers/nsq/nsq-poc/src/main/java/com/rogtejada/nsq/Consumer.java
@@ -11,10 +11,9 @@ public class Consumer {
         lookup.addLookupAddress("localhost", 4161);
         NSQConsumer consumer = new NSQConsumer(lookup, "TestTopic", "ch", (message) -> {
             System.out.println("received: " + message);
-            //now mark the message as finished.
+         
             message.finished();
-
-            //or you could requeue it,http://ba4214f85f00:4161/ which indicates a failure and puts it back on the queue.
+            
             //message.requeue();
         });
 
