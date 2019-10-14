88d292c196c8f90a9c566efadcf6578aca1d6a51
Rodrigo Tejada <rogtejada@hotmail.com>
2019-10-14 16:02:47 -0300

    Update App.java

--- brokers/kafka/kafka-POC/src/main/java/com/rogtejada/kafka/App.java
+++ brokers/kafka/kafka-POC/src/main/java/com/rogtejada/kafka/App.java
@@ -18,24 +18,24 @@ public class App {
         int noMessageFound = 0;
         while (true) {
             ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
-            // 1000 is the time in milliseconds consumer will wait if no record is found at broker.
+           
             if (consumerRecords.count() == 0) {
                 noMessageFound++;
                 if (noMessageFound > IKafkaConstants.MAX_NO_MESSAGE_FOUND_COUNT)
-                    // If no message found count is reached to threshold exit loop.
+                  
                     break;
                 else {
                     continue;
                 }
             }
-            //print each record.
+            
             consumerRecords.forEach(record -> {
                 System.out.println("Record Key " + record.key());
                 System.out.println("Record value " + record.value());
                 System.out.println("Record partition " + record.partition());
                 System.out.println("Record offset " + record.offset());
             });
-            // commits the offset of record to broker.
+           
             consumer.commitAsync();
         }
         consumer.close();
