f8c62fc861f4e619589b166bb84447b2715cbf41
rtejada <rodrigo.tejada@ilegra.com>
2019-11-10 19:03:00 -0300

    refactor kafka poc

---
+++ brokers/kafka/kafka/src/main/java/com/rtejada/poc/kafka/KafkaApplication.java
@@ -0,0 +1,13 @@
+package com.rtejada.poc.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+}
---
+++ brokers/kafka/kafka/src/main/java/com/rtejada/poc/kafka/controller/KafkaController.java
@@ -0,0 +1,25 @@
+package com.rtejada.poc.kafka.controller;
+
+import com.rtejada.poc.kafka.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    public KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
